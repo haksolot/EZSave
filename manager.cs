15ec09a80a6eadf632e75ec23015a7df46e963f0
Estelle Laulhe <estelle.laulhe@gmail.com>
2025-02-03 16:05:00 +0100

    Création de la classe manager

---
+++ manager.cs
@@ -0,0 +1,41 @@
+public class Manager
+{
+    private List<Jobs> jobs { get; set; }
+    private int limit { get; set; } = 5
+    private string logDestination { get; set; }
+
+    public void add(Jobs job) { 
+        if(jobs.Count >= limit)
+        {
+            Console.WriteLine("You have exceeded the maximum number of allowed backups (max 5 jobs) !" );
+        }
+        else
+        {
+            jobs.Add(job);
+            Console.WriteLine("The job " + job + " has been successfully added to the list !");
+        }
+    }
+
+    public void delete(Jobs job)
+    {
+        if (jobs.Contains(job))
+        {
+            jobs.Remove(job);
+            Console.WriteLine("The job " + job + " has been deleted from the list !");
+        }
+        else
+        {
+            Console.WriteLine("The job " + job + " was not found in the list !");
+        }
+    }
+
+    public execute();
+
+    public void show(List<Jobs> jobs)
+    {
+        foreach (Jobs job in jobs)
+        {
+            Console.WriteLine(job);
+        }
+    }
+}
