48d9390a12a18165bfaea04ee9f9dd0803cef8f3
Haksolot <sean.lamet.mail@gmail.com>
2025-02-06 12:06:38 +0100

    adding constructor to JobModel to simplify instanciation

--- src/Models/JobModel.cs
+++ src/Models/JobModel.cs
@@ -6,5 +6,13 @@ namespace EZSave.Core.Models
     public string Source { get; set; } = "";
     public string Destination { get; set; } = "";
     public string Type { get; set; } = "";
+
+    public JobModel(string name, string source, string destination, string type)
+    {
+      Name = name;
+      Source = source;
+      Destination = destination;
+      Type = type;
+    }
   }
 }
