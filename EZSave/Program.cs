948ef14883cb51d2adc612ada62bb6f6710952e9
Estelle Laulhe <estelle.laulhe@gmail.com>
2025-02-07 22:41:14 +0100

    adding language projet to solution

--- EZSave/Program.cs
+++ EZSave/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace EZSave
+﻿using System.Globalization;
+using EZSave.Language;
+
+namespace EZSave
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        
+            static void Main()
+            {
+                // Définir la langue par défaut
+                SetLanguage("fr");
+                Console.WriteLine(Resources.WelcomeMessage); // Affiche "Bienvenue dans notre application !"
+
+                // Changer de langue dynamiquement
+                SetLanguage("en");
+                Console.WriteLine(Resources.WelcomeMessage); // Affiche "Welcome to our application!"
+            }
+
+            static void SetLanguage(string culture)
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+            }
+        
     }
 }
