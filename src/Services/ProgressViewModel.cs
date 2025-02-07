1517964d2e79280ac19107443c8cb7c6c34b7966
BiscuitIntrusif <123597953+BiscuitIntrusif@users.noreply.github.com>
2025-02-07 14:03:00 +0100

    comment & Update ProgressViewModel.cs

--- src/Services/ProgressViewModel.cs
+++ src/Services/ProgressViewModel.cs
@@ -12,20 +12,20 @@ namespace EZSave.Progress.Services
         {
             Progress = new ProgressModel
             {
-                FilesLeft = 10,
+                FilesLeft = 10,                          //j'initialise les 2 afin d'avoir un état initial
                  TotalFileSizeLeft = 100.0f,
                 Source = "C:\\Backup\\Source",
                 Destination = "D:\\Backup\\Destination"
             };
         }
 
-        public async Task StartProgressAsync()
+        public async Task StartProgressAsync()           // simu d'un process qui réduit progressivement le nb de fichiers           //pour l'exp user pour pas bloquer l'UI
         {
             while (Progress.FilesLeft > 0)
             {
-                await Task.Delay(1000);  // normalement ca fait le trnasfert
-                Progress.FilesLeft--;
-                   Progress.TotalFileSizeLeft -= 10.0f;
+                await Task.Delay(1000);  // normalement ca fait le trnasfert //att1 sec
+                Progress.FilesLeft--;        //diminue de 1 le nb de fichiers restants        
+                   Progress.TotalFileSizeLeft -= 10.0f;   // réduit la taille des fichiers restants
             }
         }
     }
