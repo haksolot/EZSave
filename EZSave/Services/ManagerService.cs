ce44916ef06c1e749f0c69ad30acbfa2656882c7
Estelle Laulhe <estelle.laulhe@gmail.com>
2025-02-07 22:43:33 +0100

    calling ManagerModel in functions

--- EZSave/Services/ManagerService.cs
+++ EZSave/Services/ManagerService.cs
@@ -5,26 +5,26 @@ namespace EZSave.Core.Services
     public class ManagerService
     {
 
-        public bool Add(JobModel job)
+        public bool Add(JobModel job, ManagerModel manager)
         {
-            if (ManagerModel.Jobs.Count >= ManagerModel.Limit)
+            if (manager.Jobs.Count >= manager.Limit)
             {
                 return false;
                 //Console.WriteLine("You have exceeded the maximum number of allowed backups (max 5 jobs) !");
             }
             else
             {
-                ManagerModel.Jobs.Add(job);
+                manager.Jobs.Add(job);
                 return true;
                 //Console.WriteLine("The job " + job + " has been successfully added to the list !");
             }
         }
 
-        public bool Remove(JobModel job)
+        public bool Remove(JobModel job, ManagerModel manager)
         {
-            if (ManagerModel.Jobs.Contains(job))
+            if (manager.Jobs.Contains(job))
             {
-                ManagerModel.Jobs.Remove(job);
+                manager.Jobs.Remove(job);
                 //Console.WriteLine("The job " + job + " has been deleted from the list !");
                 return true;
             }
@@ -35,17 +35,18 @@ namespace EZSave.Core.Services
             }
         }
 
-        public bool Execute()
+        public bool Execute(ManagerModel manager)
         {
-            if (ManagerModel.Jobs.Count > 0)
+            if (manager.Jobs.Count > 0)
             {
-                foreach (JobModel job in ManagerModel.Jobs)
+                foreach (JobModel job in manager.Jobs)
                 {
                     var service = new JobService();
                     service.Start(job);
                     //Console.WriteLine("The job " + job + " has been started !");
-                    return true;
+                    
                 }
+                return true;
             }
             else
             {
