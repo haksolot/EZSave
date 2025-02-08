8dffe04ef179481e21a24c6b250258f6d1c6045e
Estelle Laulhe <estelle.laulhe@gmail.com>
2025-02-08 22:16:32 +0100

    language tests ok && not view model

--- EZSave.TUI/ViewModels/LanguageViewModel.cs
+++ EZSave.TUI/ViewModels/LanguageViewModel.cs
@@ -1,68 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Globalization;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using EZSave.Core.Services;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Globalization;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Text;
+//using System.Threading.Tasks;
+//using EZSave.Core.Services;
 
-namespace EZSave.TUI.ViewModels
-{
-    public class LanguageViewModel : INotifyPropertyChanged
-    {
-        private readonly ResourcesService _resourcesService;
-        private string _currentLanguage;
-        private string _translatedText;
+//namespace EZSave.TUI.ViewModels
+//{
+//    public class LanguageViewModel : INotifyPropertyChanged
+//    {
+//        private readonly ResourcesService _resourcesService;
+//        private string _currentLanguage;
+//        private string _translatedText;
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+//        public event PropertyChangedEventHandler? PropertyChanged;
 
-        // Constructeur
-        public LanguageViewModel()
-        {
-            _resourcesService = new ResourcesService();
-            _currentLanguage = CultureInfo.CurrentCulture.TwoLetterISOLanguageName; // Langue initiale
-            _translatedText = _resourcesService.GetString("WelcomeMessage"); // Message par défaut
-        }
+//        // Constructeur
+//        public LanguageViewModel()
+//        {
+//            _resourcesService = new ResourcesService();
+//            _currentLanguage = CultureInfo.CurrentCulture.TwoLetterISOLanguageName; // Langue initiale
+//            _translatedText = _resourcesService.GetString("WelcomeMessage"); // Message par défaut
+//        }
 
-        public string CurrentLanguage
-        {
-            get => _currentLanguage;
-            set
-            {
-                if (_currentLanguage != value)
-                {
-                    _currentLanguage = value;
-                    OnPropertyChanged();
-                    ChangeLanguage(_currentLanguage); 
-                }
-            }
-        }
+//        public string CurrentLanguage
+//        {
+//            get => _currentLanguage;
+//            set
+//            {
+//                if (_currentLanguage != value)
+//                {
+//                    _currentLanguage = value;
+//                    OnPropertyChanged();
+//                    ChangeLanguage(_currentLanguage); 
+//                }
+//            }
+//        }
 
-        public string TranslatedText
-        {
-            get => _translatedText;
-            set
-            {
-                if (_translatedText != value)
-                {
-                    _translatedText = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+//        public string TranslatedText
+//        {
+//            get => _translatedText;
+//            set
+//            {
+//                if (_translatedText != value)
+//                {
+//                    _translatedText = value;
+//                    OnPropertyChanged();
+//                }
+//            }
+//        }
 
-        public void ChangeLanguage(string languageCode)
-        {
-            _resourcesService.SetLanguage(languageCode);  
-            TranslatedText = _resourcesService.GetString("WelcomeMessage"); 
-        }
+//        public void ChangeLanguage(string languageCode)
+//        {
+//            _resourcesService.SetLanguage(languageCode);  
+//            TranslatedText = _resourcesService.GetString("WelcomeMessage"); 
+//        }
 
       
-        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-}
+//        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+//        {
+//            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+//        }
+//    }
+//}
--- EZSave/Services/ResourcesService.cs
+++ EZSave/Services/ResourcesService.cs
@@ -1,28 +1,29 @@
-﻿using System.Resources;
+﻿using System;
 using System.Globalization;
+using System.Resources;
+using System.Threading;
+    using EZSave.Language.Resources;
 
 namespace EZSave.Core.Services
 {
     public class ResourcesService
     {
+
         private readonly ResourceManager _resourceManager;
 
         public ResourcesService()
         {
-            _resourceManager = new ResourceManager("EZSave.Language.Resources.Resources",
-                typeof(ResourcesService).Assembly);
+            _resourceManager = new ResourceManager(typeof(AppResources));
         }
 
         public string GetString(string key)
         {
-            return _resourceManager.GetString(key) ?? $"[{key}]"; 
+            return _resourceManager.GetString(key) ?? $"[{key}]";
         }
 
-        public void SetLanguage(string languageCode)
+        public void ChangeLanguage(string cultureCode)
         {
-            CultureInfo culture = new CultureInfo(languageCode);
-            CultureInfo.CurrentCulture = culture;
-            CultureInfo.CurrentUICulture = culture;
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureCode);
         }
     }
 }
