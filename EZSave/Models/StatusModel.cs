1069125e979e10ff550f8b250155a0ea88e416bc
GahikoVON <fabien.64000@outlook.fr>
2025-02-08 16:27:25 +0100

    Ajout de la méthode SaveStatus

---
+++ EZSave/Models/StatusModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EZSave.Core.Models
+{
+    public class StatusModel
+    {
+        public string Name { get; set; } = "";
+        public string State { get; set; } = "";
+        public double Progress { get; set; }
+        public double Size { get; set; }
+        public double TotalTransferTime { get; set; }
+        public int TotalFilesToCopy { get; set; }
+    }
+}
---
+++ EZSave/Services/StatusService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using EZSave.Core.Models;
+using System.Text.Json;
+
+namespace EZSave.Core.Services
+{
+    public class StatusService
+    {
+        public void SaveStatus(StatusModel statusmodel, ConfigFileModel configmodel)
+        {
+            var liststatus = new Dictionary<string,StatusModel>();
+            string statusDirectory = configmodel.StatusFileDestination;
+            string statusFilePath = Path.Combine(statusDirectory, "_status.json");
+            //Verif dossier existe
+            if (!Directory.Exists(statusDirectory))
+            {
+                Directory.CreateDirectory(statusDirectory);
+            }
+
+            // Changer seulement les status existants  
+            if(File.Exists(statusFilePath))
+            {
+                string json = File.ReadAllText(statusFilePath);
+                if (!string.IsNullOrWhiteSpace(json))
+                {
+                    liststatus = JsonSerializer.Deserialize<Dictionary<string, StatusModel>>(json) ?? new Dictionary<string, StatusModel>();
+                }
+            }
+
+            // Verif si le statuc existe dans le fichier 
+            if (liststatus.ContainsKey(statusmodel.Name))
+            {
+                // upadte les valeurs du status
+                liststatus[statusmodel.Name] = statusmodel;
+            }
+            else
+            {
+                // sinon ajoute le statut
+                liststatus.Add(statusmodel.Name, statusmodel);
+            }
+
+            string jsonString = JsonSerializer.Serialize(liststatus, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(statusFilePath, jsonString);
+        }
+    }
+}
