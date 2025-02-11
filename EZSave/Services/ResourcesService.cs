fb8dd2875e1c818374c94a8d214eba7959ed7422
Estelle Laulhe <estelle.laulhe@gmail.com>
2025-02-11 15:11:01 +0100

    adding LanguageViewModel && LanguageTestGUI + modifying ResourcesService

--- EZSave/Services/ResourcesService.cs
+++ EZSave/Services/ResourcesService.cs
@@ -1,45 +1,56 @@
-﻿using System;
+﻿using EZSave.Language.Resources;
 using System.Collections;
 using System.Globalization;
 using System.Resources;
-using System.Threading;
-using EZSave.Language.Resources;
 
 namespace EZSave.Core.Services
 {
-  public class ResourcesService
-  {
-
-    private readonly ResourceManager _resourceManager;
-    private readonly Dictionary<string, string> _resourcesCache;
-    public ResourcesService()
+    public class ResourcesService
     {
-      _resourcesCache = new Dictionary<string, string>();
-      _resourceManager = new ResourceManager(typeof(AppResources));
-      LoadResources("fr");
+        private readonly ResourceManager _resourceManager;
+        private readonly Dictionary<string, string> _resourcesCache;
+        private CultureInfo _currentCulture;
+        public ResourcesService()
+        {
+            _resourcesCache = new Dictionary<string, string>();
+            _resourceManager = new ResourceManager(typeof(AppResources));
+            _currentCulture = new CultureInfo("fr");
+            LoadResources("fr");
+        }
+
+        public string GetString(string key)
+        {
+            return _resourceManager.GetString(key) ?? $"[{key}]";
+        }
+        public CultureInfo CurrentCulture
+        {
+            get => _currentCulture;
+            set
+            {
+                if (_currentCulture != value)
+                {
+                    _currentCulture = value;
+                    LoadResources(value.Name);  
+                }
+            }
+        }
+
+
+        public void LoadResources(string cultureCode)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureCode);
+
+            _resourcesCache.Clear();
+
+            foreach (DictionaryEntry entry in _resourceManager.GetResourceSet(Thread.CurrentThread.CurrentUICulture, true, true))
+            {
+                _resourcesCache.Add(entry.Key.ToString(), entry.Value.ToString());
+            }
+        }
+
+        public void ChangeLanguage(string cultureCode)
+        {
+            CurrentCulture = new CultureInfo(cultureCode);  
+        }
     }
-
-    public string GetString(string key)
-    {
-      return _resourceManager.GetString(key) ?? $"[{key}]";
-    }
-
-    public void LoadResources(string cultureCode)
-    {
-
-      Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureCode);
-
-      _resourcesCache.Clear();
-
-      foreach (DictionaryEntry entry in _resourceManager.GetResourceSet(Thread.CurrentThread.CurrentUICulture, true, true))
-      {
-        _resourcesCache.Add(entry.Key.ToString(), entry.Value.ToString());
-      }
-    }
-
-    public void ChangeLanguage(string cultureCode)
-    {
-      LoadResources(cultureCode);
-    }
-  }
 }
