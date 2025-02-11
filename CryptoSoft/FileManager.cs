c7a2540da2431f67e266aa83088075484e801f05
Haksolot <sean.lamet.mail@gmail.com>
2025-02-11 15:00:35 +0100

    adding modified cryptosoft version

---
+++ CryptoSoft/FileManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace CryptoSoft;
+
+public class Cipher(string path, string key)
+{
+  private string Path { get; } = path;
+  private string Key { get; } = key;
+
+  public void Process()
+  {
+    if (File.Exists(Path))
+    {
+      TransformFile(Path);
+    }
+    else if (Directory.Exists(Path))
+    {
+      TransformDirectory();
+    }
+    else
+    {
+    }
+  }
+
+  private void TransformDirectory()
+  {
+    var files = Directory.GetFiles(Path, "*", SearchOption.AllDirectories);
+    foreach (var file in files)
+    {
+      TransformFile(file);
+    }
+  }
+
+  private void TransformFile(string filePath)
+  {
+    if (!File.Exists(filePath))
+    {
+      Console.WriteLine($"File not found: {filePath}");
+      return;
+    }
+
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    var fileBytes = File.ReadAllBytes(filePath);
+    var keyBytes = ConvertToByte(Key);
+    fileBytes = XorMethod(fileBytes, keyBytes);
+    File.WriteAllBytes(filePath, fileBytes);
+    stopwatch.Stop();
+
+    Console.WriteLine($"Processed: {filePath} in {stopwatch.ElapsedMilliseconds} ms");
+  }
+
+  private static byte[] ConvertToByte(string text)
+  {
+    return Encoding.UTF8.GetBytes(text);
+  }
+
+  private static byte[] XorMethod(IReadOnlyList<byte> fileBytes, IReadOnlyList<byte> keyBytes)
+  {
+    var result = new byte[fileBytes.Count];
+    for (var i = 0; i < fileBytes.Count; i++)
+    {
+      result[i] = (byte)(fileBytes[i] ^ keyBytes[i % keyBytes.Count]);
+    }
+    return result;
+  }
+}
