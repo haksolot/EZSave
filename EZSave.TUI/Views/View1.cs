fcbaea476584a4ee2223a2ed2abb1d348e52a96d
Haksolot <sean.lamet.mail@gmail.com>
2025-02-12 14:26:17 +0100

    Adding log config type menu

--- EZSave.TUI/Views/View1.cs
+++ EZSave.TUI/Views/View1.cs
@@ -3,142 +3,148 @@ using EZSave.TUI.ViewModels;
 
 namespace EZSave.TUI.Views
 {
-    public class View1
+  public class View1
+  {
+    private readonly ViewModel1 _viewModel;
+    private readonly ResourcesService _resourcesService;
+
+    public View1(ViewModel1 viewModel, ResourcesService resourcesService)
     {
-        private readonly ViewModel1 _viewModel;
-        private readonly ResourcesService _resourcesService;
+      _viewModel = viewModel;
+      _resourcesService = resourcesService;
+      viewModel.Initialize();
+    }
 
-        public View1(ViewModel1 viewModel, ResourcesService resourcesService)
-        {
-            _viewModel = viewModel;
-            _resourcesService = resourcesService;
-            viewModel.Initialize();
-        }
+    public void ShowJobs()
+    {
+      Console.Write(_viewModel.GetJobs());
+    }
+    public void Display()
+    {
+      bool inConfigMode = false;
 
-        public void ShowJobs()
+      while (true)
+      {
+        Console.Clear();
+        if (inConfigMode)
         {
-            Console.Write(_viewModel.GetJobs());
-        }
-        public void Display()
-        {
-            bool inConfigMode = false;
+          Console.WriteLine(_viewModel.ConfModeTitle);
+          for (int i = 0; i < _viewModel.ConfigOptions.Count; i++)
+          {
+            Console.WriteLine($"{i + 1}. {_viewModel.ConfigOptions[i]}");
+          }
 
-            while (true)
+          Console.Write(_viewModel.ChoiceTitle);
+          if (int.TryParse(Console.ReadLine(), out int configChoice))
+          {
+            if (configChoice == 1)
             {
-                Console.Clear();
-                if (inConfigMode)
-                {
-                    Console.WriteLine(_viewModel.ConfModeTitle);
-                    for (int i = 0; i < _viewModel.ConfigOptions.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}. {_viewModel.ConfigOptions[i]}");
-                    }
+              Console.Write(_viewModel.EnterJobName);
+              string name = Console.ReadLine();
 
-                    Console.Write(_viewModel.ChoiceTitle);
-                    if (int.TryParse(Console.ReadLine(), out int configChoice))
-                    {
-                        if (configChoice == 1)
-                        {
-                            Console.Write(_viewModel.EnterJobName);
-                            string name = Console.ReadLine();
+              Console.Write(_viewModel.EnterSource);
+              string source = Console.ReadLine();
 
-                            Console.Write(_viewModel.EnterSource);
-                            string source = Console.ReadLine();
+              Console.Write(_viewModel.EnterDestination);
+              string destination = Console.ReadLine();
 
-                            Console.Write(_viewModel.EnterDestination);
-                            string destination = Console.ReadLine();
+              Console.Write(_viewModel.EnterType);
+              string type = Console.ReadLine();
 
-                            Console.Write(_viewModel.EnterType);
-                            string type = Console.ReadLine();
+              bool resultAdd = _viewModel.AddJob(name, source, destination, type);
+              if (resultAdd)
+              {
+                Console.WriteLine(_viewModel.JobAdded);
+              }
+              else
+              {
+                Console.WriteLine(_viewModel.JobNotAdded);
+              }
+            }
+            if (configChoice == 2)
+            {
+              Console.WriteLine(_viewModel.ListJobsPossibleModify);
+              ShowJobs();
+              Console.Write(_viewModel.EnterModifiedJob);
+              string jobName = Console.ReadLine();
 
-                            bool resultAdd = _viewModel.AddJob(name, source, destination, type);
-                            if (resultAdd)
-                            {
-                                Console.WriteLine(_viewModel.JobAdded);
-                            }
-                            else
-                            {
-                                Console.WriteLine(_viewModel.JobNotAdded);
-                            }
-                        }
-                        if (configChoice == 2)
-                        {
-                            Console.WriteLine(_viewModel.ListJobsPossibleModify);
-                            ShowJobs();
-                            Console.Write(_viewModel.EnterModifiedJob);
-                            string jobName = Console.ReadLine();
+              Console.Write(_viewModel.EnterSource);
+              string source = Console.ReadLine();
 
-                            Console.Write(_viewModel.EnterSource);
-                            string source = Console.ReadLine();
+              Console.Write(_viewModel.EnterDestination);
+              string destination = Console.ReadLine();
 
-                            Console.Write(_viewModel.EnterDestination);
-                            string destination = Console.ReadLine();
+              Console.Write(_viewModel.EnterType);
+              string type = Console.ReadLine();
 
-                            Console.Write(_viewModel.EnterType);
-                            string type = Console.ReadLine();
+              bool resultEdit = _viewModel.EditJob(jobName, source, destination, type);
+              if (resultEdit)
+              {
+                Console.WriteLine(_viewModel.JobEdited);
+              }
+              else
+              {
+                Console.WriteLine(_viewModel.JobNotEdited);
+              }
+            }
+            if (configChoice == 3)
+            {
+              Console.WriteLine(_viewModel.ListJobsPossibleDelete);
+              ShowJobs();
 
-                            bool resultEdit = _viewModel.EditJob(jobName, source, destination, type);
-                            if (resultEdit)
-                            {
-                                Console.WriteLine(_viewModel.JobEdited);
-                            }
-                            else
-                            {
-                                Console.WriteLine(_viewModel.JobNotEdited);
-                            }
-                        }
-                        if (configChoice == 3)
-                        {
-                            Console.WriteLine(_viewModel.ListJobsPossibleDelete);
-                            ShowJobs();
+              Console.Write(_viewModel.EnterDeletedJob);
+              string jobName = Console.ReadLine();
 
-                            Console.Write(_viewModel.EnterDeletedJob);
-                            string jobName = Console.ReadLine();
-
-                            bool resultDelete = _viewModel.DeleteJob(jobName);
-                            if (resultDelete)
-                            {
-                                Console.WriteLine(_viewModel.JobDeleted);
-                            }
-                            else
-                            {
-                                Console.WriteLine(_viewModel.JobNotDeleted);
-                            }
-                        }
-                        if (configChoice == 4)
-                        {
-                            Console.WriteLine(_viewModel.LogPathChanging);
-                            string dest = Console.ReadLine();
-                            _viewModel.ChangeLogPath(dest);
-                        }
-                        if (configChoice == 5)
-                        {
-                            Console.WriteLine(_viewModel.ConfigFilePathChanging);
-                            string dest = Console.ReadLine();
-                            _viewModel.ChangeConfigPath(dest);
-                        }
-                        if (configChoice == 6)
-                        {
-                            Console.WriteLine(_viewModel.StatusFilePathChanging);
-                            string dest = Console.ReadLine();
-                            _viewModel.ChangeStatusPath(dest);
-                        }
-                        else
-                        {
-                            if (configChoice == 7)
-                            {
-                                inConfigMode = false;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine(_viewModel.InvalidChoiceTitle);
-                    }
-                }
-                else
-                {
-                    Console.Write(@"
+              bool resultDelete = _viewModel.DeleteJob(jobName);
+              if (resultDelete)
+              {
+                Console.WriteLine(_viewModel.JobDeleted);
+              }
+              else
+              {
+                Console.WriteLine(_viewModel.JobNotDeleted);
+              }
+            }
+            if (configChoice == 4)
+            {
+              Console.WriteLine(_viewModel.LogPathChanging);
+              string dest = Console.ReadLine();
+              _viewModel.ChangeLogPath(dest);
+            }
+            if (configChoice == 5)
+            {
+              Console.WriteLine(_viewModel.LogTypeChanging);
+              string type = Console.ReadLine();
+              _viewModel.ChangeLogType(type);
+            }
+            if (configChoice == 6)
+            {
+              Console.WriteLine(_viewModel.ConfigFilePathChanging);
+              string dest = Console.ReadLine();
+              _viewModel.ChangeConfigPath(dest);
+            }
+            if (configChoice == 7)
+            {
+              Console.WriteLine(_viewModel.StatusFilePathChanging);
+              string dest = Console.ReadLine();
+              _viewModel.ChangeStatusPath(dest);
+            }
+            else
+            {
+              if (configChoice == 8)
+              {
+                inConfigMode = false;
+              }
+            }
+          }
+          else
+          {
+            Console.WriteLine(_viewModel.InvalidChoiceTitle);
+          }
+        }
+        else
+        {
+          Console.Write(@"
 
  _____ ______ _____
 |  ___|___  //  ___|
@@ -148,71 +154,68 @@ namespace EZSave.TUI.Views
 \____/\_____/\____/ \__,_| \_/ \___|
 
 ");
-                    Console.WriteLine(_viewModel.MainMenuTitle);
-                    for (int i = 0; i < _viewModel.MainOptions.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}. {_viewModel.MainOptions[i]}");
-                    }
+          Console.WriteLine(_viewModel.MainMenuTitle);
+          for (int i = 0; i < _viewModel.MainOptions.Count; i++)
+          {
+            Console.WriteLine($"{i + 1}. {_viewModel.MainOptions[i]}");
+          }
 
-                    Console.Write(_viewModel.ChoiceTitle);
-                    if (int.TryParse(Console.ReadLine(), out int mainChoice))
-                    {
-                        if (mainChoice == 1)
-                        {
-                            bool value = _viewModel.ExecuteJobs();
-                            if (value)
-                            {
-                                Console.WriteLine(_viewModel.JobsExecutedSuccess);
-                            }
-                            else
-                            {
-                                Console.WriteLine(_viewModel.JobsExecutedFail);
-                            }
-
-                            //_viewModel.ExecuteMainOption(mainChoice);
-                        }
-                        else if (mainChoice == 2)
-                        {
-                            //_viewModel.ExecuteMainOption(mainChoice);
-                            inConfigMode = true;
-                        }
-                        else if (mainChoice == 3)
-                        {
-                            for (int i = 0; i < _viewModel.LanguageOptions.Count; i++)
-                            {
-                                Console.WriteLine($"{i + 1}. {_viewModel.LanguageOptions[i]}");
-                            }
-                            Console.Write(_viewModel.ChoiceTitle);
-                            if (int.TryParse(Console.ReadLine(), out int languageChoice))
-                            {
-                                if (languageChoice == 1)
-                                {
-                                    _viewModel.ChangeLanguage("fr");
-                                }
-                                else if (languageChoice == 2)
-                                {
-                                    _viewModel.ChangeLanguage("en");
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine(_viewModel.InvalidChoiceTitle);
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine(_viewModel.InvalidChoiceTitle);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine(_viewModel.InvalidChoiceTitle);
-                    }
-                }
-
-                Console.WriteLine(_viewModel.ChooseChoiceTitle);
-                Console.ReadKey();
+          Console.Write(_viewModel.ChoiceTitle);
+          if (int.TryParse(Console.ReadLine(), out int mainChoice))
+          {
+            if (mainChoice == 1)
+            {
+              bool value = _viewModel.ExecuteJobs();
+              if (value)
+              {
+                Console.WriteLine(_viewModel.JobsExecutedSuccess);
+              }
+              else
+              {
+                Console.WriteLine(_viewModel.JobsExecutedFail);
+              }
             }
+            else if (mainChoice == 2)
+            {
+              inConfigMode = true;
+            }
+            else if (mainChoice == 3)
+            {
+              for (int i = 0; i < _viewModel.LanguageOptions.Count; i++)
+              {
+                Console.WriteLine($"{i + 1}. {_viewModel.LanguageOptions[i]}");
+              }
+              Console.Write(_viewModel.ChoiceTitle);
+              if (int.TryParse(Console.ReadLine(), out int languageChoice))
+              {
+                if (languageChoice == 1)
+                {
+                  _viewModel.ChangeLanguage("fr");
+                }
+                else if (languageChoice == 2)
+                {
+                  _viewModel.ChangeLanguage("en");
+                }
+              }
+              else
+              {
+                Console.WriteLine(_viewModel.InvalidChoiceTitle);
+              }
+            }
+            else
+            {
+              Console.WriteLine(_viewModel.InvalidChoiceTitle);
+            }
+          }
+          else
+          {
+            Console.WriteLine(_viewModel.InvalidChoiceTitle);
+          }
         }
+
+        Console.WriteLine(_viewModel.ChooseChoiceTitle);
+        Console.ReadKey();
+      }
     }
+  }
 }
--- EZSave/Services/ConfigService.cs
+++ EZSave/Services/ConfigService.cs
@@ -3,95 +3,105 @@ using EZSave.Core.Models;
 
 namespace EZSave.Core.Services
 {
-    public class ConfigService
+  public class ConfigService
+  {
+    public void SetConfigDestination(string dest, ConfigFileModel config)
     {
-        public void SetConfigDestination(string dest, ConfigFileModel config)
-        {
-            config.ConfFileDestination = dest;
-            string newJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
-            if (!string.IsNullOrEmpty(config.ConfFileDestination))
-            {
-                File.WriteAllText(config.ConfFileDestination, newJson);
-            }
-        }
-
-        public void SetLogDestination(string dest, ConfigFileModel config)
-        {
-            config.LogFileDestination = dest;
-            string newJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
-            if (!string.IsNullOrEmpty(config.ConfFileDestination))
-            {
-                File.WriteAllText(config.ConfFileDestination, newJson);
-            }
-        }
-
-        public void SetStatusDestination(string dest, ConfigFileModel config)
-        {
-            config.StatusFileDestination = dest;
-            string newJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
-            if (!string.IsNullOrEmpty(config.ConfFileDestination))
-            {
-                File.WriteAllText(config.ConfFileDestination, newJson);
-            }
-        }
-
-        public bool LoadConfigFile(ConfigFileModel config)
-        {
-            if (!string.IsNullOrEmpty(config.ConfFileDestination) && File.Exists(config.ConfFileDestination))
-            {
-                string json = File.ReadAllText(config.ConfFileDestination);
-                ConfigFileModel? tempConfig = JsonSerializer.Deserialize<ConfigFileModel>(json) ?? new ConfigFileModel();
-                if (tempConfig == null)
-                {
-                    return false;
-                }
-                config.ConfFileDestination = tempConfig.ConfFileDestination;
-                config.LogFileDestination = tempConfig.LogFileDestination;
-                config.Jobs = tempConfig.Jobs != null ? new Dictionary<string, JobModel>(tempConfig.Jobs) : new Dictionary<string, JobModel>();
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public bool SaveJob(JobModel job, ConfigFileModel config)
-        {
-            if (File.Exists(config.ConfFileDestination))
-            {
-                string json = File.ReadAllText(config.ConfFileDestination);
-                config = JsonSerializer.Deserialize<ConfigFileModel>(json) ?? new ConfigFileModel();
-            }
-            else
-            {
-                config = new ConfigFileModel();
-            }
-
-            config.Jobs ??= new Dictionary<string, JobModel>();
-            config.Jobs[job.Name] = job;
-            string newJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
-
-            if (!string.IsNullOrEmpty(config.ConfFileDestination))
-            {
-                File.WriteAllText(config.ConfFileDestination, newJson);
-                return true;
-            }
-            return false;
-        }
-
-
-        public void DeleteJob(JobModel job, ConfigFileModel config)
-        {
-            if (!string.IsNullOrEmpty(config.ConfFileDestination) && File.Exists(config.ConfFileDestination))
-            {
-                string json = File.ReadAllText(config.ConfFileDestination);
-                ConfigFileModel deserializedConfig = JsonSerializer.Deserialize<ConfigFileModel>(json) ?? new ConfigFileModel();
-                deserializedConfig.Jobs?.Remove(job.Name);
-                string updatedJson = JsonSerializer.Serialize(deserializedConfig, new JsonSerializerOptions { WriteIndented = true });
-                File.WriteAllText(config.ConfFileDestination, updatedJson);
-            }
-        }
+      config.ConfFileDestination = dest;
+      string newJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+      if (!string.IsNullOrEmpty(config.ConfFileDestination))
+      {
+        File.WriteAllText(config.ConfFileDestination, newJson);
+      }
     }
+
+    public void SetLogDestination(string dest, ConfigFileModel config)
+    {
+      config.LogFileDestination = dest;
+      string newJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+      if (!string.IsNullOrEmpty(config.ConfFileDestination))
+      {
+        File.WriteAllText(config.ConfFileDestination, newJson);
+      }
+    }
+
+    public void SetLogType(string type, ConfigFileModel config)
+    {
+      config.LogType = type;
+      string newJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+      if (!string.IsNullOrEmpty(config.ConfFileDestination))
+      {
+        File.WriteAllText(config.ConfFileDestination, newJson);
+      }
+    }
+
+    public void SetStatusDestination(string dest, ConfigFileModel config)
+    {
+      config.StatusFileDestination = dest;
+      string newJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+      if (!string.IsNullOrEmpty(config.ConfFileDestination))
+      {
+        File.WriteAllText(config.ConfFileDestination, newJson);
+      }
+    }
+
+    public bool LoadConfigFile(ConfigFileModel config)
+    {
+      if (!string.IsNullOrEmpty(config.ConfFileDestination) && File.Exists(config.ConfFileDestination))
+      {
+        string json = File.ReadAllText(config.ConfFileDestination);
+        ConfigFileModel? tempConfig = JsonSerializer.Deserialize<ConfigFileModel>(json) ?? new ConfigFileModel();
+        if (tempConfig == null)
+        {
+          return false;
+        }
+        config.ConfFileDestination = tempConfig.ConfFileDestination;
+        config.LogFileDestination = tempConfig.LogFileDestination;
+        config.Jobs = tempConfig.Jobs != null ? new Dictionary<string, JobModel>(tempConfig.Jobs) : new Dictionary<string, JobModel>();
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    public bool SaveJob(JobModel job, ConfigFileModel config)
+    {
+      if (File.Exists(config.ConfFileDestination))
+      {
+        string json = File.ReadAllText(config.ConfFileDestination);
+        config = JsonSerializer.Deserialize<ConfigFileModel>(json) ?? new ConfigFileModel();
+      }
+      else
+      {
+        config = new ConfigFileModel();
+      }
+
+      config.Jobs ??= new Dictionary<string, JobModel>();
+      config.Jobs[job.Name] = job;
+      string newJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+
+      if (!string.IsNullOrEmpty(config.ConfFileDestination))
+      {
+        File.WriteAllText(config.ConfFileDestination, newJson);
+        return true;
+      }
+      return false;
+    }
+
+
+    public void DeleteJob(JobModel job, ConfigFileModel config)
+    {
+      if (!string.IsNullOrEmpty(config.ConfFileDestination) && File.Exists(config.ConfFileDestination))
+      {
+        string json = File.ReadAllText(config.ConfFileDestination);
+        ConfigFileModel deserializedConfig = JsonSerializer.Deserialize<ConfigFileModel>(json) ?? new ConfigFileModel();
+        deserializedConfig.Jobs?.Remove(job.Name);
+        string updatedJson = JsonSerializer.Serialize(deserializedConfig, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(config.ConfFileDestination, updatedJson);
+      }
+    }
+  }
 }
 
