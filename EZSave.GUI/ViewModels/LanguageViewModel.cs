bc81e66e0d65179d7facf54ccfc6630fa1004fcf
Estelle Laulhe <estelle.laulhe@gmail.com>
2025-02-12 16:18:32 +0100

    adding refresh and adding jobs buttons

---
+++ EZSave.GUI/ViewModels/LanguageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using EZSave.GUI.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace EZSave.GUI.ViewModels
+{
+    public class LanguageViewModel
+    {
+        public ICommand ChangeLanguageCommand { get; }
+
+        public LanguageViewModel()
+        {
+            ChangeLanguageCommand = new RelayCommand<string>(ChangeLanguage);
+        }
+
+        private void ChangeLanguage(string languageCode)
+        {
+            try
+            {
+                CultureInfo newCulture = new CultureInfo(languageCode);
+                CultureInfo.CurrentCulture = newCulture;
+                CultureInfo.CurrentUICulture = newCulture;
+                Resources.Culture = newCulture;
+
+                foreach (var window in Application.Current.Windows)
+                {
+                    if (window is Window w)
+                    {
+                        w.Language = XmlLanguage.GetLanguage(newCulture.Name);
+
+                        // 🔥 Mise à jour des bindings dynamiques
+                        BindingOperations.GetBindingExpressionBase(w, Window.LanguageProperty)?.UpdateTarget();
+                    }
+                }
+            }
+            catch (CultureNotFoundException)
+            {
+            }
+        }
+
+
+
+        public string LocalizedText => Resources.ConfigOption1;
+    }
+}
---
+++ EZSave.GUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using EZSave.Core.Models;
+using EZSave.Core.Services;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.WindowsAPICodePack.Dialogs;
+namespace EZSave.GUI.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public LanguageViewModel LanguageViewModel { get; set; }
+        public ConfigFileModel configFileModel { get; set; }
+        public ManagerService managerService;
+
+        private readonly ManagerModel managerModel;
+
+        public IEnumerable<JobModel> jobs;
+        public ICommand SelectFolderCommand { get; }
+        public IEnumerable<JobModel> Jobs
+        {
+            get => jobs;
+            set => SetProperty(ref jobs, value);
+        }
+
+        public ICommand RefreshCommand { get; set; }
+        public ICommand AddJobCommand { get; set; }
+        public ICommand OpenJobWindowCommand { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+      
+
+
+        public MainWindowViewModel()
+        {
+            LanguageViewModel = new LanguageViewModel();
+
+            managerModel = new ManagerModel();
+            RefreshCommand = new RelayCommand(RefreshJobs);
+            OpenJobWindowCommand = new RelayCommand(OpenAddJobWindow);
+            //Jobs = managerModel.Jobs;
+        }
+
+       
+
+        private void SetProperty<T>(ref T old, T @new, [CallerMemberName] string name = "")
+        {
+            old = @new;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void RefreshJobs()
+        {
+            Jobs = managerModel.Jobs.ToList(); 
+        }
+
+
+        private void OpenAddJobWindow()
+        {
+            var window = new AddJobWindow(managerModel);  
+            window.ShowDialog();
+        }
+
+     
+        
+
+    }
+}
